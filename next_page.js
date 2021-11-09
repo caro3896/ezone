dfe2e8d00928ecd6babddab5f5bc5cffeff77e7c
Caroline <caro713f@stud.kea.dk>
2021-11-09 18:04:45 +0100

    go back button works

--- next_page.js
+++ next_page.js
@@ -15,6 +15,7 @@ function userInfo() {
   document.querySelector("#gerenal_info").classList.add("hide");
   document.querySelector("#user_info").classList.remove("hide");
   document.querySelector("#next2").addEventListener("click", personalInfo);
+  document.querySelector("#back1").addEventListener("click", goBack1);
 }
 
 function personalInfo() {
@@ -22,9 +23,20 @@ function personalInfo() {
   document.querySelector("#user_info").classList.add("hide");
   document.querySelector("#personal_info").classList.remove("hide");
   document.querySelector("#add").addEventListener("click", thankYou);
+  document.querySelector("#back2").addEventListener("click", goBack2);
 }
 
 function thankYou() {
   console.log("User signed up!");
   document.querySelector("#personal_info").classList.add("hide");
+}
+
+function goBack1(){
+  document.querySelector("#user_info").classList.add("hide");
+  document.querySelector("#gerenal_info").classList.remove("hide");
+}
+
+function goBack2(){
+  document.querySelector("#personal_info").classList.add("hide");
+  document.querySelector("#user_info").classList.remove("hide");
 }

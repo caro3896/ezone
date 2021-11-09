1a011916a880f1ed2bd128a4007c387d0152db55
Caroline <caro713f@stud.kea.dk>
2021-11-09 12:42:31 +0100

    id on buttons

--- next_page.js
+++ next_page.js
@@ -7,21 +7,21 @@ export function start() {
 function gerenalInfo() {
   console.log("Join butter clicked! General info");
   document.querySelector("#gerenal_info").classList.remove("hide");
-  document.querySelector("#gerenal_info button").addEventListener("click", userInfo);
+  document.querySelector("#next1").addEventListener("click", userInfo);
 }
 
 function userInfo() {
   console.log("User info!");
   document.querySelector("#gerenal_info").classList.add("hide");
   document.querySelector("#user_info").classList.remove("hide");
-  document.querySelector("#user_info button").addEventListener("click", personalInfo);
+  document.querySelector("#next2").addEventListener("click", personalInfo);
 }
 
 function personalInfo() {
   console.log("Personal info!");
   document.querySelector("#user_info").classList.add("hide");
   document.querySelector("#personal_info").classList.remove("hide");
-  document.querySelector("#personal_info button").addEventListener("click", thankYou);
+  document.querySelector("#add").addEventListener("click", thankYou);
 }
 
 function thankYou() {

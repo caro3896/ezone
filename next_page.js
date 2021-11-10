ecc9b362d711dae637d5211b66bf08071bd888ba
Caroline <caro713f@stud.kea.dk>
2021-11-10 12:48:05 +0100

    transistion on modals

--- next_page.js
+++ next_page.js
@@ -10,6 +10,7 @@ export function start() {
 function gerenalInfo() {
   console.log("Join butter clicked! General info");
   document.querySelector("#gerenal_info").classList.remove("hide");
+  document.querySelector("#gerenal_info").classList.add("active");
 
   // Valider before clicking on next
   const form = document.querySelector("form");
@@ -17,7 +18,6 @@ function gerenalInfo() {
   document.querySelector("#next1").addEventListener("click", (e) => {
     e.preventDefault();
     if (form.checkValidity()) {
-      console.log("hej");
       userInfo();
     } else {
       form.reportValidity();
@@ -27,35 +27,45 @@ function gerenalInfo() {
 
 function userInfo() {
   console.log("User info!");
+  document.querySelector("#gerenal_info").classList.remove("active");
   document.querySelector("#gerenal_info").classList.add("hide");
   document.querySelector("#user_info").classList.remove("hide");
+  document.querySelector("#user_info").classList.add("active");
   document.querySelector("#next2").addEventListener("click", personalInfo);
   document.querySelector("#back1").addEventListener("click", goBack1);
 }
 
 function personalInfo() {
   console.log("Personal info!");
+  document.querySelector("#user_info").classList.remove("active");
   document.querySelector("#user_info").classList.add("hide");
   document.querySelector("#personal_info").classList.remove("hide");
+  document.querySelector("#personal_info").classList.add("active");
   document.querySelector("#add").addEventListener("click", thankYou);
   document.querySelector("#back2").addEventListener("click", goBack2);
 }
 
 function thankYou() {
   console.log("User signed up!");
+  document.querySelector("#personal_info").classList.remove("active");
   document.querySelector("#personal_info").classList.add("hide");
   document.querySelector("#signed_up").classList.remove("hide");
+  document.querySelector("#signed_up").classList.add("active");
   document.querySelector("#home").addEventListener("click", close);
 }
 
 function goBack1() {
+  document.querySelector("#user_info").classList.remove("active");
   document.querySelector("#user_info").classList.add("hide");
   document.querySelector("#gerenal_info").classList.remove("hide");
+  document.querySelector("#gerenal_info").classList.add("active");
 }
 
 function goBack2() {
+  document.querySelector("#personal_info").classList.remove("active");
   document.querySelector("#personal_info").classList.add("hide");
   document.querySelector("#user_info").classList.remove("hide");
+  document.querySelector("#user_info").classList.add("active");
 }
 
 function close() {
@@ -63,5 +73,6 @@ function close() {
   document.querySelectorAll(".modal").forEach((modal) => {
     console.log("close modal");
     modal.classList.add("hide");
+    modal.classList.remove("active");
   });
 }
